07f733de33e11728f5efc07224c2e81d4c01d00f
Maria Princess Santillan <mariaprincess.santillan@infor.com>
2021-06-13 18:02:05 +0800

    Grid first comment

--- src/script.js
+++ src/script.js
@@ -3,8 +3,13 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Texture loader
+const loader = new THREE.TextureLoader()
+const cross = loader.load('./GridProfile.png')
+
 // Debug
 const gui = new dat.GUI()
+dat.GUI.toggleHide();
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -13,16 +18,63 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.TorusGeometry( .7, .2, 20, 80 );
+
+const particlesGeometry = new THREE.BufferGeometry;
+const particlesCnt = 5000;
+
+const posArray = new Float32Array(particlesCnt * 3);
+
+for(let i = 0; i < particlesCnt * 3; i++){
+    //posArray[i] = Math.random() - 0.5
+
+    posArray[i] = (Math.random() - 0.5) * (Math.random() * 5)
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3))
+
+// Profile Geometry
+const profileGeometry = new THREE.BufferGeometry;
+const profileCnt = 300;
+
+const profArray = new Float32Array(profileCnt * 3);
+
+for(let i = 0; i < profileCnt * 3; i++){
+    //posArray[i] = Math.random() - 0.5
+
+    profArray[i] = (Math.random() - 0.8) * (Math.random() * 8)
+}
+
+profileGeometry.setAttribute('position', new THREE.BufferAttribute(profArray, 3))
+
+
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.PointsMaterial({
+    size: 0.005
+})
+
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.0005,
+    // map: cross,
+    // transparent: true,
+    color: 'cyan'
+})
+
+const particlesProfile = new THREE.PointsMaterial({
+    size: 0.05,
+    map: cross,
+    transparent: true,
+    color: 'cyan'
+})
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const sphere = new THREE.Points(geometry,material)
+const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial)
+const particlesProfileFloat = new THREE.Points(profileGeometry, particlesProfile)
+scene.add(particlesMesh, particlesProfileFloat)
+//scene.add(sphere, particlesMesh, particlesProfileFloat)
 
 // Lights
 
@@ -77,6 +129,19 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(new THREE.Color('#21282a'), 1)
+
+/* Mouse */
+
+document.addEventListener('mousemove', animateParticles)
+
+let mouseX = 0
+let mouseY = 0
+
+function animateParticles(event){
+    mouseY = event.clientY
+    mouseX = event.clientX
+}
 
 /**
  * Animate
@@ -91,7 +156,16 @@ const tick = () =>
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
+    particlesMesh.rotation.y = -.1 * elapsedTime
+    particlesProfileFloat.rotation.y = -.1 * elapsedTime
 
+    if(mouseX > 0){
+    particlesMesh.rotation.y = mouseX * (elapsedTime * 0.00008)
+    particlesMesh.rotation.x = -mouseY * (elapsedTime * 0.00008)
+
+    particlesProfileFloat.rotation.y = mouseX * (elapsedTime * 0.00008)
+    particlesProfileFloat.rotation.x = -mouseY * (elapsedTime * 0.00008)
+    }
     // Update Orbital Controls
     // controls.update()
 
